a8f4146fe96a018cdc370b132acf7b13706f5322
jzafvt771 <ijmm7171@gmail.com>
2023-08-16 19:46:49 -0500

    Fix upload

--- script.js
+++ script.js
@@ -1,63 +1,59 @@
-
 const fileInput = document.getElementById('fileInput');
 const fileInfo = document.getElementById('fileInfo');
 const preview = document.getElementById('preview');
 const uploadProgress = document.getElementById('uploadProgress');
 
 fileInput.addEventListener('change', function(event) {
-  const selectedFile = event.target.files[0];
+    const selectedFile = event.target.files[0];
 
-  const maxSize = 4 * 1024 * 1024; // 4MB in bytes
-  if (selectedFile.size > maxSize) {
-    fileInfo.innerHTML = 'El archivo es demasiado grande. Por favor, selecciona un archivo de máximo 4MB.';
-    preview.style.display = 'none';
-    return;
-  }
-
-  const allowedExtensions = ['.pdf', '.jpg', '.jpeg', '.png', '.gif']; // Add more if needed
-  const fileExtension = selectedFile.name.split('.').pop().toLowerCase();
-  if (!allowedExtensions.includes('.' + fileExtension)) {
-    fileInfo.innerHTML = 'Formato de archivo no válido. Solo se permiten archivos PDF o imágenes.';
-    preview.style.display = 'none';
-    return;
-  }
-
-  fileInfo.innerHTML = `
-    Nombre del archivo: ${selectedFile.name}<br>
-    Tipo MIME: ${selectedFile.type}<br>
-    Tamaño: ${selectedFile.size} bytes
-  `;
-
-  const isImage = selectedFile.type.startsWith('image/');
-  
-  if (isImage) {
-    preview.style.display = 'block';
-    const reader = new FileReader();
-    reader.onload = function(e) {
-      preview.src = e.target.result;
-    };
-    reader.readAsDataURL(selectedFile);
-  } else {
-    preview.style.display = 'none';
-  }
-
-  // Upload using Axios
-  const formData = new FormData();
-  formData.append('file', selectedFile);
-
-  axios.post('/upload', formData, {
-    onUploadProgress: function(progressEvent) {
-      const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
-      uploadProgress.style.display = 'block';
-      uploadProgress.value = progress;
+    const maxSize = 4 * 1024 * 1024; // 4MB in bytes
+    if (selectedFile.size > maxSize) {
+        fileInfo.innerHTML = 'The file is too large. Please select a file of maximum 4MB.';
+        preview.style.display = 'none';
+        return;
     }
-  })
-  .then(response => {
-    // Handle the response from the server
-    console.log('Upload successful:', response.data);
-  })
-  .catch(error => {
-    // Handle upload error
-    console.error('Upload error:', error);
-  });
+
+    const allowedExtensions = ['.pdf', '.jpg', '.jpeg', '.png', '.gif'];
+    const fileExtension = selectedFile.name.split('.').pop().toLowerCase();
+    if (!allowedExtensions.includes('.' + fileExtension)) {
+        fileInfo.innerHTML = 'Invalid file format. Only PDF or image files are allowed.';
+        preview.style.display = 'none';
+        return;
+    }
+
+    fileInfo.innerHTML = `
+        File name: ${selectedFile.name}<br>
+        MIME type: ${selectedFile.type}<br>
+        Size: ${selectedFile.size} bytes
+    `;
+
+    const isImage = selectedFile.type.startsWith('image/');
+
+    if (isImage) {
+        preview.style.display = 'block';
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+        };
+        reader.readAsDataURL(selectedFile);
+    } else {
+        preview.style.display = 'none';
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    axios.post('http://localhost:3000/upload', formData, {
+        onUploadProgress: function(progressEvent) {
+            const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+            uploadProgress.style.display = 'block';
+            uploadProgress.value = progress;
+        }
+    })
+    .then(response => {
+        console.log('Upload successful:', response.data.message);
+    })
+    .catch(error => {
+        console.error('Upload error:', error);
+    });
 });
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
+
 const app = express();
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
+app.use(fileUpload());
 
-// Define your other routes and server logic here
+app.post('/upload', (req, res) => {
+    if (!req.files || !req.files.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+
+    let EDFile = req.files.file;
+
+    EDFile.mv(`./files/${EDFile.name}`, err => {
+        if (err) return res.status(500).send({ message: err });
+
+        return res.status(200).send({ message: 'File uploaded successfully.' });
+    });
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server running on port 3000');
 });
